7b063821e05471bcbcf2af911c736d901a40803d
Julio Fernando Gomez Bobadilla <jgomezbo@emeal.nttdata.com>
2022-08-29 19:49:27 -0500

    Proyecto version V5.0

--- Bootcamp/ProyectoV2.0/1.Account/src/main/java/com/webflux/jfgb/webflux/AccountApplication.java
+++ Bootcamp/ProyectoV2.0/1.Account/src/main/java/com/webflux/jfgb/webflux/AccountApplication.java
@@ -3,6 +3,7 @@ package com.webflux.jfgb.webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @EnableEurekaClient
 @SpringBootApplication
 public class AccountApplication {
